4f83645473fb92cf071e0c64009a5ecfbe236133
JoakimCarlsen <Joakim@h53.aau1x-2.klient.sydhavn.site.aau.dk>
2016-11-11 10:44:25 +0100

    Made a master from both Game and Networking. Nicolais menu is not in yet though

---
+++ PCSS_Project_Master/Assets/Scripts/Client.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+[System.Serializable]
+public class Client  {
+
+    public string IP;
+    public string clientName;
+
+    public Client(string newIP, string newClientName)
+    {
+        this.IP = newIP;
+        this.clientName = newClientName;
+    }
+	
+}
