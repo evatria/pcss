ae838905d22536a55302f028d59edc2fb615722a
alexanderlarsen <alexanderlarsen89@gmail.com>
2017-10-31 15:05:19 +0100

    Client changes

--- ClientServerMultithreading/ClientApplication/Program.cs
+++ ClientServerMultithreading/ClientApplication/Program.cs
@@ -11,34 +11,30 @@ public class EchoClient
         Console.Write("Please enter your screen name and press enter: \n");
         String screenName = Console.ReadLine();
         Console.Write("\nWelcome " + screenName + ". You are now connected to the server.");
-        Console.Write("\nWrite r and press enter when you are ready \n");
 
+        try {
         TcpClient client = new TcpClient("127.0.0.1", 10000);
         StreamReader reader = new StreamReader(client.GetStream());
         StreamWriter writer = new StreamWriter(client.GetStream());
 
-        String readyCheck = Console.ReadLine();
+        /*String s = String.Empty;
+        s = Console.ReadLine();
+        Console.Write("\nEnter a string to send to the server: ");*/
 
-        while(readyCheck.Equals("r")) {
-            try {
-               
-                String s = String.Empty;
+        Console.Write("\nYou are connected to the server. Waiting for other players.");
+        
+        Console.WriteLine();
+        writer.WriteLine(screenName);
+        writer.Flush();
+        String server_string = reader.ReadLine();
+        Console.WriteLine(server_string);
 
-                Console.Write("\nYou are connected to the server. Waiting for other players.");
-                Console.Write("\nEnter a string to send to the server: ");
-                s = Console.ReadLine();
-                Console.WriteLine();
-                writer.WriteLine(s);
-                writer.Flush();
-                String server_string = reader.ReadLine();
-                Console.WriteLine(server_string);
+        reader.Close();
+        writer.Close();
+        client.Close();
 
-                reader.Close();
-                writer.Close();
-                client.Close();
-            } catch (Exception e) {
-                Console.WriteLine(e);
-            }
+        } catch (Exception e) {
+            Console.WriteLine(e);
         }
     }
 }
