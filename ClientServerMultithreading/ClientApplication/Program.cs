eaa0630ddd1d13c4cb1f1e6fe112f7b69c12c9e7
bjornSkalkam <bjorn@skalkam.dk>
2017-10-31 16:54:42 +0100

    A lot of changes

--- ClientServerMultithreading/ClientApplication/Program.cs
+++ ClientServerMultithreading/ClientApplication/Program.cs
@@ -4,18 +4,19 @@ using System.Net;
 using System.Net.Sockets;
 
 public class EchoClient {
+    static Boolean selectedId = false;
 public static void Main() {
 
-Console.Write("Press 0 to create a server. Press any other button to see a list of servers to join.");
+/*Console.Write("Press 0 to create a server. Press any other button to see a list of servers to join.");
     String x = Console.ReadLine();
+
     if (x == "0") {
             Console.Write("Creating server");
             String j = Console.ReadLine();
     } else {
             Console.Write("List of servers: \n");
             String j = Console.ReadLine();
-    }
-   
+    }*/
 
     try {
     TcpClient client = new TcpClient("127.0.0.1", 10000);
@@ -24,15 +25,22 @@ Console.Write("Press 0 to create a server. Press any other button to see a list 
     String s = String.Empty;
 
     while (!s.Equals("Exit")) {
-        Console.Write("Enter a string to send to the server: ");
+        if (!selectedId) {
+
+        Console.Write("Connected to server! \n");
+        Console.Write("Select a unique ID: ");
         s = Console.ReadLine();
+
         Console.WriteLine();
         writer.WriteLine(s);
+        selectedId = true;
+        }
         writer.Flush();
         String server_string = reader.ReadLine();
         Console.WriteLine("from server: " + server_string + "\n");
+         
         }
-
+    
     reader.Close();
     writer.Close();
     client.Close();
--- ClientServerMultithreading/ServerApplication/Program.cs
+++ ClientServerMultithreading/ServerApplication/Program.cs
@@ -6,22 +6,74 @@ using System.Threading;
 
 public class MultiThreadedEchoServer
 {
-    static int totalNumberOfAs = 0;
+    //static int totalNumberOfAs = 0;
+    static String id1 = "noIdYet";
+    static String id2 = "noIdYet";
+    static String id3 = "noIdYet";
+    static Boolean threePlayersConnected = false;
 
     private static void ProcessClientRequests(object argument)
     {
+        
         TcpClient client = (TcpClient)argument;
         try {
+            
             StreamReader reader = new StreamReader(client.GetStream());
             StreamWriter writer = new StreamWriter(client.GetStream());
             string s = String.Empty;
             while (!(s = reader.ReadLine()).Equals("Exit") || (s == null)) {
-                Console.WriteLine("Client sends the following string: " + s);
-                int count = s.Split('a').Length - 1;
-                totalNumberOfAs = totalNumberOfAs + count;
-                Console.WriteLine("The accumulated number of a's found in all messages from all client threads so far: " + totalNumberOfAs + "\n");
-                writer.WriteLine("The letter 'a' was found " + count + " times in the string you sent the server.");
+                if (!threePlayersConnected) {
+                //Console.Write("input: " + s + "\n");
+                Console.Write("ID: " + s + "\n");
+                Console.Write("\n");
+
+                if (id1 == "noIdYet") {
+                    id1 = s;
+                    Console.Write("player 1: " + id1 + "\n");
+                        Console.WriteLine ("Waiting for more players. \n");
+                } else if (id1 != "noIdYet" && id2 == "noIdYet") {
+                    id2 = s;
+                    Console.Write("player 2: " + id2 + "\n");
+                        Console.WriteLine ("Waiting for more players. \n");
+                } else if (id2 != "noIdYet" && id3 == "noIdYet") {
+                    id3 = s;
+                    Console.Write("player 3: " + id3 + "\n");
+                        Console.WriteLine ("Found three players! \n");
+                }
+
+                if (id1 != "noIdYet" && id2 != "noIdYet" && id3 != "noIdYet") {
+                    Console.Write("3 players connected!" + "\n");
+                    threePlayersConnected = true;
+                    Console.Write("Player 1: " + id1 + "\n");
+                    Console.Write("Player 2: " + id2 + "\n");
+                    Console.Write("Player 3: " + id3 + "\n");
+                }
+                if (threePlayersConnected) {
+                    Console.WriteLine ("Starting Game!");
+                }
+                
                 writer.Flush();
+                }
+                
+
+                /*if (s[0] == '0') {
+                    Console.WriteLine("CREATING LOBBY 0");
+                    writer.WriteLine("CREATING LOBBY 0");
+                } else if (s[0] == '1') {
+                    Console.WriteLine("JOINING LOBBY 0");
+                    writer.WriteLine("JOINING LOBBY 0");
+                } else {
+                    Console.WriteLine("DID NOT RECOGNIZE COMMAND");
+                    writer.WriteLine("DID NOT RECOGNIZE COMMAND");
+                }*/
+
+                //Console.WriteLine("Client sends the following string: " + s);
+                //int count = s.Split('a').Length - 1;
+
+                //totalNumberOfAs = totalNumberOfAs + count;
+                //Console.WriteLine("The accumulated number of a's found in all messages from all client threads so far: " + totalNumberOfAs + "\n");
+                //writer.WriteLine("The letter 'a' was found " + count + " times in the string you sent the server.");
+                
             }
             reader.Close();
             writer.Close();
@@ -44,9 +96,9 @@ public class MultiThreadedEchoServer
             listener.Start();
             Console.WriteLine("MultiThreadedEchoServer started...");
             while (true) {
-                Console.WriteLine("Waiting for client...");
+                Console.WriteLine("Waiting for client... \n");
                 TcpClient client = listener.AcceptTcpClient();
-                Console.WriteLine("Accepted new client...");
+                Console.WriteLine("Accepted new client... \n");
                 Thread t = new Thread(ProcessClientRequests);
                 t.Start(client);
             }
