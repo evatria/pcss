5fa31c1b961cb65fb53b2aca31841d8bb62bca9b
bjornSkalkam <bjorn@skalkam.dk>
2017-10-31 15:36:20 +0100

    tests

--- ClientServerMultithreading/ClientApplication/Program.cs
+++ ClientServerMultithreading/ClientApplication/Program.cs
@@ -3,29 +3,42 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 
-public class EchoClient
-{
-    public static void Main()
-    {
-        try {
-            TcpClient client = new TcpClient("127.0.0.1", 10000);
-            StreamReader reader = new StreamReader(client.GetStream());
-            StreamWriter writer = new StreamWriter(client.GetStream());
-            String s = String.Empty;
-            while (!s.Equals("Exit")) {
-                Console.Write("Enter a string to send to the server: ");
-                s = Console.ReadLine();
-                Console.WriteLine();
-                writer.WriteLine(s);
-                writer.Flush();
-                String server_string = reader.ReadLine();
-                Console.WriteLine("from server: " + server_string + "\n");
-            }
-            reader.Close();
-            writer.Close();
-            client.Close();
-        } catch (Exception e) {
-            Console.WriteLine(e);
-        }
+public class EchoClient {
+public static void Main() {
+
+Console.Write("Press 0 to create a server. Press any other button to see a list of servers to join.");
+    String x = Console.ReadLine();
+    if (x == "0") {
+            Console.Write("Creating server");
+            String j = Console.ReadLine();
+    } else {
+            Console.Write("List of servers: \n");
+            String j = Console.ReadLine();
     }
+   
+
+    try {
+    TcpClient client = new TcpClient("127.0.0.1", 10000);
+    StreamReader reader = new StreamReader(client.GetStream());
+    StreamWriter writer = new StreamWriter(client.GetStream());
+    String s = String.Empty;
+
+    while (!s.Equals("Exit")) {
+        Console.Write("Enter a string to send to the server: ");
+        s = Console.ReadLine();
+        Console.WriteLine();
+        writer.WriteLine(s);
+        writer.Flush();
+        String server_string = reader.ReadLine();
+        Console.WriteLine("from server: " + server_string + "\n");
+        }
+
+    reader.Close();
+    writer.Close();
+    client.Close();
+
+    } catch (Exception e) {
+    Console.WriteLine(e);
+    }
+}
 }
