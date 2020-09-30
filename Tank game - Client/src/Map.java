0cd823fe2a39783885cb84a6a62a2f1ba67dd13e
AtleNyhus <55181788+AtleNyhus@users.noreply.github.com>
2020-09-30 15:12:53 +0200

    ting og sager

--- Tank game - Client/src/Map.java
+++ Tank game - Client/src/Map.java
@@ -1,2 +1,21 @@
-public class Map {
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.swing.*;
+
+public class Map extends JPanel {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("My Drawing");
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize(720,720);
+        ImageIcon pic = new ImageIcon("path");
+        Image tank = pic.getImage();
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
 }
+
