049a4b91d37ba922bd70004371eaa57b3adf5bcf
AtleNyhus <55181788+AtleNyhus@users.noreply.github.com>
2020-10-12 14:40:02 +0200

    projectile

--- Tank game - Client/src/Projectile.java
+++ Tank game - Client/src/Projectile.java
@@ -1,129 +1,42 @@
 //package sample;
 import javafx.scene.shape.Circle;
 
-public class Projectile {
+public class Projectile extends Circle {
 
     final static int MAX_SPEED = 10;
-    final static int[] BULLET_COLOR = {20,20,20};
+    final static int[] BULLET_COLOR = {20, 20, 20};
+    static final int HITBOX_RADIUS = 5;
 
-    private Tank player = new Tank();
-    private Tank[] enemies;
-
+    private Tank player;
+    private Circle c = new Circle();
     private int xPos;
     private int yPos;
     private int xSpeed;
     private int ySpeed;
-    private int direction;
     private int hitboxRadius;
     private boolean hit = false;
 
-    static final int HITBOX_RADIUS = 5;
+    Projectile(Tank tank){
+        create();
+        setSpeed(tank);
+    }
+
+
+    void create(){
+        this.c = new Circle(tank.getX()+Math.cos(tank.getAngle())*5., tank.getY()+Math.sin(tank.getAngel())*5, HITBOX_RADIUS);
+    }
+
+    void setSpeed(Tank tank){
+        xSpeed =  MAX_SPEED * Math.cos(tank.getAngle());
+        ySpeed = MAX_SPEED * Math.sin(tank.getAngle());
+    }
+    void moveBullet() {
+
+    }
+
 
 
     private Circle hitboxCircle;
 
 
-    Projectile(){}
-    Projectile(Tank player_, Tank[] enemies_, int xPos_, int yPos){
-        setPlayer(player_);
-        setEnemies(enemies_);
-        hitbox(xPos_, yPos);
-    }
-
-
-
-
-
-    public void hitbox(int x, int y){
-        this.hitboxCircle = new Circle(getxPos(),getyPos(),this.HITBOX_RADIUS);
-    }
-
-    public void updateHitbox(int x, int y){
-        this.hitboxCircle.setCenterX(this.hitboxCircle.getCenterX() + getxSpeed());
-        this.hitboxCircle.setCenterY(this.hitboxCircle.getCenterY() + getySpeed());
-    }
-
-
-
-
-  /*  public boolean move(){
-        setxPos(getxPos() + getxSpeed());
-        setyPos(getyPos() + getySpeed());
-
-        if (getxPos() == getEnemies().getXpos){
-            this.hit = true;
-            return true;
-        }
-        return false;
-    }
-    */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public void setPlayer(Tank player) {
-        this.player = player;
-    }
-
-    public void setEnemies(Tank[] enemies) {
-        this.enemies = enemies;
-    }
-
-    public Tank[] getEnemies() {
-        return enemies;
-    }
-
-    public void setxPos(int xPos) {
-        this.xPos = xPos;
-    }
-    public int getxPos() {
-        return xPos;
-    }
-
-    public void setyPos(int yPos) {
-        this.yPos = yPos;
-    }
-    public int getyPos() {
-        return yPos;
-    }
-
-    public void setDirection(int direction) {
-        this.direction = direction;
-    }
-    public int getDirection() {
-        return direction;
-    }
-
-    public void setxSpeed(int xSpeed) {
-        this.xSpeed = xSpeed;
-    }
-    public int getxSpeed() {
-        return xSpeed;
-    }
-
-    public void setySpeed(int ySpeed) {
-        this.ySpeed = ySpeed;
-    }
-    public int getySpeed() {
-        return ySpeed;
-    }
 }
