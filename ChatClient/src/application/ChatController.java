da76aad92110af4d9b9d1bfc1cf55e5807f8ca1c
ineffablejoy <ineffablejoy@users.noreply.github.com>
2020-10-28 14:24:27 +0100

    Chat Interface v1
    Chat GUI, xml and basic functions for sending/displaying a message.

--- ChatClient/src/application/ChatController.java
+++ ChatClient/src/application/ChatController.java
@@ -5,12 +5,21 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class ChatController implements Initializable{
@@ -32,6 +41,50 @@ public class ChatController implements Initializable{
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
 		
+		roomNametxt.setText("Chad Room"); 							//**Test for changing Chat Room name**
+		
 	}
 
+	//FXML imports 
+	@FXML
+	private ListView<String> chatDisplayList;
+	
+	@FXML
+	private Button sendbtn;
+	
+	@FXML
+	private TextArea chatField;
+	
+	@FXML
+	private Text roomNametxt;
+	
+
+	
+	
+	//Send by pressing the button
+	public void sendbtn(ActionEvent event) {
+		sendMessage();
+	}
+	
+	//Send by pressing "Enter"
+	public void sendField(KeyEvent keyEvent) {
+				
+		if (keyEvent.getCode() == KeyCode.ENTER) {
+			sendMessage();
+		}
+	}
+
+	//Method for sending message to list
+	public void sendMessage() {
+		String message = chatField.getText();
+		
+		if (message != null) {
+			chatField.setText(null);
+			this.chatDisplayList.getItems().add(message);
+		}
+		
+		else {
+			System.out.println("No message sent. Message was: " + message);
+		}
+	}
 }
