ec1db8db24b6603e5017c97486505d6ff008b202
ineffablejoy <ineffablejoy@users.noreply.github.com>
2020-10-28 15:35:29 +0100

    New user popup and controller
    Still needs work. Right now it accepts any input.

--- ChatClient/src/application/NewUserController.java
+++ ChatClient/src/application/NewUserController.java
@@ -4,31 +4,48 @@ package application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class NewUserController {
 	
 	@FXML
-	private TextField userNameInput;
+	private TextField newUserNameInput;
 	
 	@FXML
-	private TextField passWordInput;
+	private PasswordField newPassWordInput;
 	
 	@FXML
-	private Button createUserBtn;
+	private Button newCreateUserBtn;
+	
+	@FXML
+	private Text popErrorNameTxt;
 	
 	private String userPass;
 	
-	public void login(ActionEvent event)
+	public void createNewUser(ActionEvent event)
 	{
-		if (this.userNameInput != null && this.passWordInput.getText() != null) 
+		
+		if (newUserNameInput.getText() == null && newUserNameInput.getPromptText() != null || newPassWordInput.getText() == null && newPassWordInput.getPromptText() != null){
+			popErrorNameTxt.setText("Test");
+			System.out.println("Error");
+		}
+		
+		
+		else if (this.newUserNameInput.getText() != null && this.newUserNameInput.getText() != null) 
 		{
-			setUserPass(this.userNameInput.getText()+ ("|") + this.passWordInput.getText());
+			setUserPass(this.newUserNameInput.getText()+ ("|") + this.newPassWordInput.getText());
+			System.out.println("It saved... "+ userPass);
+			((Stage)(((Button)event.getSource()).getScene().getWindow())).close(); 
 		}
 
-		((Stage)(((Button)event.getSource()).getScene().getWindow())).close(); 
+
+
+
 	}
+
 
 	public String getUserPass() {
 		return userPass;
