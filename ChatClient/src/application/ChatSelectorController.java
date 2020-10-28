ce91014a1f78b51b584952f9f73adf8384bf5da8
ineffablejoy <ineffablejoy@users.noreply.github.com>
2020-10-28 11:40:31 +0100

    ChatSelector v1
    Chat Selector XML and test functions

--- ChatClient/src/application/ChatSelectorController.java
+++ ChatClient/src/application/ChatSelectorController.java
@@ -4,14 +4,21 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Callback;
+
 
 public class ChatSelectorController implements Initializable{
 	
@@ -31,7 +38,46 @@ public class ChatSelectorController implements Initializable{
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		
+
+		/*
+		workNow.setItems(ChatRoomTempObservableList);
+		workNow.setCellFactory(new Callback<ListView<ChatRoomTemp>, ListCell<ChatRoomTemp>>() {
+		    @Override
+		    public ListCell<ChatRoomTemp> call(ListView<ChatRoomTemp> ChatRoomListView) {
+		        return new ListCell<ChatRoomTemp>();
+		    }
+		});
+		*/
 	}
 
+	@FXML
+	private Button createRoomBtn;
+	
+	@FXML
+	private ListView<String> roomList;
+	
+	/*
+	private ObservableList<ChatRoomTemp> ChatRoomTempObservableList;
+
+	public ChatSelectorController() {
+		
+		ChatRoomTempObservableList = FXCollections.observableArrayList();
+		
+		ChatRoomTempObservableList.addAll(
+				new ChatRoomTemp("Test", "Test discription"),
+				new ChatRoomTemp("Test2", "Test discription"),
+				new ChatRoomTemp("Test3", "Test discription")
+				);
+	}
+	*/
+
+
+	public void createRoomBtn(ActionEvent event) {
+		
+		System.out.println("Room btn works");
+		this.roomList.getItems().add("Room");
+	}
+
+
+	
 }
