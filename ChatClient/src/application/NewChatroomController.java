7b29633023f3420c093b6936f710c0dcd4285a56
ineffablejoy <ineffablejoy@users.noreply.github.com>
2020-10-29 14:25:37 +0100

    Constraint & Error to chat creation

--- ChatClient/src/application/NewChatroomController.java
+++ ChatClient/src/application/NewChatroomController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class NewChatroomController {
@@ -14,17 +15,33 @@ public class NewChatroomController {
 	@FXML
 	private TextField chatName;
 	
+	@FXML
+	private Text popErrorRoomTxt;
+	
 	private String newChatName;
 	
 	public void create(ActionEvent event)
 	{
-		if (this.chatName != null ) 
+		if (chatName.getText().trim().isEmpty() || chatName.getPromptText() != null ) {
+			System.out.println("Error");
+			popErrorRoomTxt.setText("Error: Missing room name");
+			popErrorRoomTxt.setVisible(true);
+		}
+		
+		else if (this.chatName.getText() != null && this.chatName.getText() != null) 
 		{
 			this.setNewChatName(this.chatName.getText());
+			((Stage)(((Button)event.getSource()).getScene().getWindow())).close(); 
 		}
 		
 
-		((Stage)(((Button)event.getSource()).getScene().getWindow())).close(); 
+
+	}
+	
+	public void roomExists()
+	{
+		popErrorRoomTxt.setText("Error: Room name already exist");
+		popErrorRoomTxt.setVisible(true);
 	}
 
 	public String getNewChatName() {
@@ -34,4 +51,6 @@ public class NewChatroomController {
 	public void setNewChatName(String newChatName) {
 		this.newChatName = newChatName;
 	}
+	
+	
 }
