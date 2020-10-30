fce2e41ee69c3f0b7d027ff7fbb3079ecb0234a1
frederikDarling <56248424+frederikDarling@users.noreply.github.com>
2020-10-30 11:40:06 +0100

    user deleted

--- Server/src/Chatroom.java
+++ Server/src/Chatroom.java
@@ -3,46 +3,61 @@ import java.util.Random;
 
 public class Chatroom {
 	
+	// The name of the chatroom
 	private String name;
 	
+	// The id for the chatroom 
 	private int chatId;
 	
+	// An ArrayList that stores all of the users in the chatroom
 	private ArrayList<User> users = new ArrayList<>();
 	
+	// A User object that stores the user that created the chatroom
 	private User creator;
 	
-	//private ChatHistory chatHistory;
+	// a ChatHistory object used to store the chat history of the chatroom
+	private ChatHistory chatHistory;
 	
+	// The constructor for the Chatroom class that creates a Chatroom where the user creating the chatroom is assigned the creator 
+	// and where the chatroom is given a specific name
 	public Chatroom(User user, String name){
 		
 		this.creator = user;
 		this.name = name;
 		
+		// A random integer is assigned as the id for the chatroom
 		Random rd = new Random();
         this.chatId = rd.nextInt();
 	}
 	
+	// Method that returns the name of the chatroom
 	public String getChatroomName() {
 		
 		return this.name;
 		
 	}
 	
+	// Method that returns the unique id of the chatroom
 	public int getChatId() {
 		
 		return chatId;
 	}
 	
+	// Method that returns the name of the creator of the chatroom 
     public String getCreatorName() {
     	
 		return this.creator.getUsername();
 		
 	}
     
+    // Method that returns of list of usernames of the users in the chatroom
     public ArrayList<String> getUsernames() {
     	
+    	
+    	// An ArrayList for the usernames is created
     	ArrayList<String> usernames = new ArrayList<>();
     	
+    	// A for each loop goes through the user and gets their usernames
     	for(User i: users) {
     		usernames.add(i.getUsername());
     	}
@@ -51,12 +66,14 @@ public class Chatroom {
  		
  	}
 	
+    // Method that is used to display a message
 	public String displayMessage(User user, String message) {
 		
 		return user.getUsername() + ": " + message;
 		
 	}
 	
+	// Method that is used to send a message
 	public void sendMessage(String message) {
 		
 		//chatHistory.add(displayMessage());
--- Server/src/ChatroomManager.java
+++ Server/src/ChatroomManager.java
@@ -2,34 +2,41 @@ import java.util.ArrayList;
 
 public class ChatroomManager {
 	
-	
+	// An ArrayList of chatrooms is created
 	private ArrayList<Chatroom> chatrooms = new ArrayList<>();
 	
 	
-public ArrayList<Chatroom> getChatrooms() {
+	// A method that returns an ArrayList with all the chatrooms
+    public ArrayList<Chatroom> getChatrooms() {
 		
-		return chatrooms;
+	    	return chatrooms;
 
-		}
+    }
 	
-	
+	// A method that creates a new chatroom
 	public void createChatroom(User user, String name) {
 		
+		// A new chatroom object is declared
 		Chatroom newChatroom;
 		
+		// The new chatroom object is instantiated
 		newChatroom = new Chatroom(user,name);
 		
+		// The new chatroom is added to the ArrayList of chatrooms
 		chatrooms.add(newChatroom);
 		
 	}
 	
+	// A method that deletes the chatroom chosen by the user
 	public void deleteChatroom(Chatroom chatroom) {
 		
+		// A for each loop that goes through all of the chatrooms
 		for(Chatroom i : chatrooms) {
-	
 			
+			// An if-statement that checks whether the id of the chosen user matches the id of any of the chatrooms in the database
 			if(i.getChatId() == chatroom.getChatId()) {
 				
+				// The chatroom with the matching id is removed
 				chatrooms.remove(i);
 
 		}
